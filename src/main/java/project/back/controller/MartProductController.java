6cbd975c252288f92311613739ae656cd2f73066
ExpeditionMoon <dnjswon1025@gmail.com>
2024-05-10 01:21:47 +0900

    feat(product): ApiResponse 클래스를 적용하여 API 응답 형식 통일
    #13

--- src/main/java/project/back/controller/MartProductController.java
+++ src/main/java/project/back/controller/MartProductController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.back.dto.ApiResponse;
 import project.back.service.MartProductService;
 
 import java.util.Map;
@@ -18,8 +19,8 @@ public class MartProductController {
 
     /** 장바구니에 있는 모든 상품이 존재하는 마트 목록 + 마트별 합계 출력 */
     @GetMapping("/selling")
-    public ResponseEntity<Map<Long, Long>> getMartsForCart() {
-        Map<Long, Long> productTotalPrices = martProductService.findMartsByProductIds();
+    public ResponseEntity<ApiResponse<Map<Long, Long>>> getMartsForCart() {
+        ApiResponse<Map<Long, Long>> productTotalPrices = martProductService.findMartsByProductIds();
         return ResponseEntity.ok(productTotalPrices);
     }
 }
--- src/main/java/project/back/service/MartProductService.java
+++ src/main/java/project/back/service/MartProductService.java
@@ -3,6 +3,7 @@ package project.back.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.back.dto.ApiResponse;
 import project.back.dto.DiscountInfoDto;
 import project.back.entity.Mart;
 import project.back.entity.Product;
@@ -22,7 +23,7 @@ public class MartProductService {
 
     /** 장바구니에 있는 모든 상품이 존재하는 마트 목록 + 마트별 합계 출력 */
     @Transactional(readOnly = true)
-    public Map<Long, Long> findMartsByProductIds() {
+    public ApiResponse<Map<Long, Long>> findMartsByProductIds() {
         // TODO 헤더에서 memberId 가져오는 코드로 수정
         Long memberId = 1L;
         // 장바구니에 있는 상품 목록 가져오기
@@ -34,7 +35,7 @@ public class MartProductService {
         // 마트별로 할인 적용하여 상품 가격 출력
         List<DiscountInfoDto> discountInfoList = martProductRepository.findDiscountsByProductsAndMarts(productList, martList);
         // 마트별로 합계 출력(martId, totalPrice)
-        return calculateTotalFinalPriceByMart(discountInfoList);
+        return ApiResponse.success(calculateTotalFinalPriceByMart(discountInfoList), "마트별 총 합계를 불러왔습니다.");
     }
 
     private Map<Long, Long> calculateTotalFinalPriceByMart(List<DiscountInfoDto> discountInfoDto) {
