dd72c61cd835586cf6f96200ca114f5109de4ed4
김정규 <111268088+gyu0918@users.noreply.github.com>
2024-05-10 18:41:17 +0900

    apiKey 암호화 추가

--- src/test/java/project/back/configuration/JasyptConfigTest.java
+++ src/test/java/project/back/configuration/JasyptConfigTest.java
@@ -10,17 +10,25 @@ class JasyptConfigTest {
         String url = "jdbc:mysql://localhost:3306/testdb";
         String username = "root";
         String password = "rlawjdrb123";
+        //kakao api 개인 restkey
+        String restKey = "b97cb92610acfa6862b281d12658b0fc";
 
         String encryptUrl = jasyptEncrypt(url);
         String encryptUsername = jasyptEncrypt(username);
         String encryptPassword = jasyptEncrypt(password);
 
+        //kakao api 개인 restkey 부분
+        String encryptRestKey = jasyptEncrypt(restKey);
+
         System.out.println("encryptUrl : " + encryptUrl);
         System.out.println("encryptUsername : " + encryptUsername);
         System.out.println("encryptPassword : " + encryptPassword);
 
+        //kakao api 개인 restkey 부분
+        System.out.println("encryptRestKey : " + encryptRestKey);
+
         Assertions.assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
-    }
+       }
 
     private String jasyptEncrypt(String input) {
         String key = "5678";
