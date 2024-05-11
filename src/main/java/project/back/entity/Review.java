392bcfe03badddccce7bc9b800e8313b1b2c65ef
김정규 <111268088+gyu0918@users.noreply.github.com>
2024-05-11 20:27:44 +0900

    리뷰crud

--- src/main/java/project/back/entity/Review.java
+++ src/main/java/project/back/entity/Review.java
@@ -1,12 +1,14 @@
 package project.back.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Entity
-@Data
+@Getter
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +33,12 @@ public class Review {
     private Mart mart;
 
 
-
-
+    @Builder
+    public Review(Long reviewId,String reviewContent, BigDecimal score, Member member, Mart mart) {
+        this.reviewId = reviewId;
+        this.reviewContent = reviewContent;
+        this.score = score;
+        this.member = member;
+        this.mart = mart;
+    }
 }
---
+++ src/main/java/project/back/repository/reviewrepository/ReviewRepository.java
@@ -0,0 +1,13 @@
+package project.back.repository.reviewrepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.back.dto.ReviewDto;
+import project.back.entity.Review;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review,Long> {
+    List<Review> findByMartId(Long martId);
+}
