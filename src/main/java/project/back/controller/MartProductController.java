feaabc485b34444949630a8aed3a501f7df7514c
ExpeditionMoon <dnjswon1025@gmail.com>
2024-05-13 11:59:02 +0900

    refactor(product): TODO 해결 및 중복 코드 메서드로 분리
    - TODO(헤더에서 memberId 가져오는 코드로 수정) 해결
    - 중복 코드 메서드로 분리 : 사용자 확인 및 해당 사용자의 상품 목록과 각 상품의 마트별 할인 정보 반환하는 코드
    
    #17

--- src/main/java/project/back/controller/MartProductController.java
+++ src/main/java/project/back/controller/MartProductController.java
@@ -1,5 +1,6 @@
 package project.back.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,17 +23,22 @@ public class MartProductController {
 
     /** 장바구니에 있는 모든 상품이 존재하는 마트 목록 + 마트별 합계 출력 */
     @GetMapping
-    public ResponseEntity<ApiResponse<Map<Long, Long>>> getMartsForCart() {
-        ApiResponse<Map<Long, Long>> productTotalPrices = martProductService.findMartsByProductIds();
+    public ResponseEntity<ApiResponse<Map<Long, Long>>> getMartsForCart(
+            HttpServletRequest request
+    ) {
+        ApiResponse<Map<Long, Long>> productTotalPrices =
+                martProductService.findMartsByProductIds(request);
         return ResponseEntity.ok(productTotalPrices);
     }
 
     /** 마트별 상품 가격 세부사항 조회 */
     @GetMapping("/{martId}")
     public ResponseEntity<ApiResponse<List<DiscountInfoDto>>> getMartInfoForMartId(
-            @PathVariable("martId") Long martId
+            @PathVariable("martId") Long martId,
+            HttpServletRequest request
     ) {
-        ApiResponse<List<DiscountInfoDto>> martInfo = martProductService.findMartInfoByMartId(martId);
+        ApiResponse<List<DiscountInfoDto>> martInfo =
+                martProductService.findMartInfoByMartId(martId, request);
         return ResponseEntity.ok(martInfo);
     }
 }
--- src/main/java/project/back/service/MartProductService.java
+++ src/main/java/project/back/service/MartProductService.java
@@ -1,6 +1,7 @@
 package project.back.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import project.back.dto.ProductAndDiscountDataDto;
 import project.back.entity.Mart;
 import project.back.entity.Member;
 import project.back.entity.Product;
+import project.back.etc.aboutlogin.JwtUtill;
 import project.back.etc.commonException.NoContentFoundException;
 import project.back.repository.CartRepository;
 import project.back.repository.MartProductRepository;
@@ -30,6 +32,7 @@ public class MartProductService {
     private final CartRepository cartRepository;
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
+    private final JwtUtill jwtUtill;
 
     /**
      * 장바구니에 있는 모든 상품이 존재하는 마트 목록 + 마트별 합계 출력
@@ -39,46 +42,44 @@ public class MartProductService {
      * @throws NoContentFoundException 장바구니에 데이터가 존재하지 않거나 비어있을 때 발생
      */
     @Transactional(readOnly = true)
-    public ApiResponse<Map<Long, Long>> findMartsByProductIds() {
-        // TODO 헤더에서 memberId 가져오는 코드로 수정
-        Long memberId = 1L;
+    public ApiResponse<Map<Long, Long>> findMartsByProductIds(HttpServletRequest request) {
+        ProductAndDiscountDataDto loadData = getProductAndDiscountData(request);
 
-        // 회원 정보 확인
-        Optional<Member> member = memberRepository.findById(memberId);
-        if (member.isEmpty()) {
-            throw new EntityNotFoundException("존재하지 않는 회원입니다.");
-        }
-
-        /* 마트별로 합계 출력(martId, totalPrice) */
-        ProductAndDiscountDataDto loadData = loadProductsAndDiscounts(memberId);
         return ApiResponse.success(calculateTotalFinalPriceByMart(
-                loadData.getDiscountInfoList(), loadData.getCartProductList()), "마트별 총 합계를 불러왔습니다.");
+                loadData.getDiscountInfoList(),
+                loadData.getCartProductList()),
+                "마트별 총 합계를 불러왔습니다.");
     }
 
     /**
      * 마트별 상품 가격 세부사항 조회
      *
      * @return 선택된 마트의 상품의 세부 정보를 포함하는 객체를 ApiResponse 형태로 반환
-     * @throws EntityNotFoundException 사용자 엔티티를 찾을 수 없을 때 발생
+     * @throws EntityNotFoundException 사용자 정보를 찾을 수 없을 때 발생
      * @throws NoContentFoundException 장바구니에 데이터가 존재하지 않거나 비어있을 때 발생
      */
     @Transactional(readOnly = true)
-    public ApiResponse<List<DiscountInfoDto>> findMartInfoByMartId(Long martId) {
-        // TODO 헤더에서 memberId 가져오는 코드로 수정
-        Long memberId = 1L;
+    public ApiResponse<List<DiscountInfoDto>> findMartInfoByMartId(Long martId, HttpServletRequest request) {
+        ProductAndDiscountDataDto loadData = getProductAndDiscountData(request);
+        List<DiscountInfoDto> filteredDiscountInfo = loadData.getDiscountInfoList().stream()
+                .filter(dto -> dto.getMartId().equals(martId))
+                .toList();
+
+        return ApiResponse.success(filteredDiscountInfo, "마트 상품의 세부사항을 불러왔습니다.");
+    }
+
+    /** 사용자를 확인하고, 해당 사용자의 상품 목록과 각 상품의 마트별 할인 정보 반환 */
+    private ProductAndDiscountDataDto getProductAndDiscountData(HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization").substring(7);
+        Long memberId = jwtUtill.getidfromtoken(accessToken);
 
         // 회원 정보 확인
         Optional<Member> member = memberRepository.findById(memberId);
         if (member.isEmpty()) {
-            throw new EntityNotFoundException("존재하지 않는 회원입니다.");
+            throw new EntityNotFoundException("사용자 정보를 찾을 수 없습니다.");
         }
 
-        /* 해당 마트의 상품 세부사항 출력 */
-        ProductAndDiscountDataDto loadData = loadProductsAndDiscounts(memberId);
-        List<DiscountInfoDto> filteredDiscountInfo = loadData.getDiscountInfoList().stream()
-                .filter(dto -> dto.getMartId().equals(martId))
-                .toList();
-        return ApiResponse.success(filteredDiscountInfo, "마트 상품의 세부사항을 불러왔습니다.");
+        return loadProductsAndDiscounts(memberId);
     }
 
     /** memberId로 해당 회원의 장바구니에 있는 상품 목록과 각 상품의 마트별 할인 정보를 반환 */
@@ -89,6 +90,7 @@ public class MartProductService {
         if (cartProductList.isEmpty()) {
             throw new NoContentFoundException("장바구니가 존재하지 않습니다.");
         }
+
         List<Product> productList = cartProductList.stream()
                 .map(dto -> productRepository.findById(dto.getProductId()))
                 .flatMap(Optional::stream)
@@ -99,10 +101,12 @@ public class MartProductService {
         }
         Long numberOfProducts = (long) productList.size();
 
-        List<Mart> martList = martProductRepository.findMartsSellingAllProducts(productList, numberOfProducts);
+        List<Mart> martList =
+                martProductRepository.findMartsSellingAllProducts(productList, numberOfProducts);
 
         /* 마트별로 할인 적용하여 상품 가격 출력 */
-        List<DiscountInfoDto> discountInfoList = martProductRepository.findDiscountsByProductsAndMarts(productList, martList);
+        List<DiscountInfoDto> discountInfoList =
+                martProductRepository.findDiscountsByProductsAndMarts(productList, martList);
 
         return new ProductAndDiscountDataDto(productList, discountInfoList, cartProductList);
     }
@@ -113,13 +117,17 @@ public class MartProductService {
             List<CartProductDto> cartProductList
     ) {
         Map<Long, Long> productIdToQuantity = cartProductList.stream()
-                .collect(Collectors.toMap(CartProductDto::getProductId, CartProductDto::getQuantity, (a, b) -> b));
+                .collect(Collectors.toMap(
+                        CartProductDto::getProductId,
+                        CartProductDto::getQuantity,
+                        (a, b) -> b));
 
         return discountInfoDto.stream()
                 .filter(dto -> productIdToQuantity.containsKey(dto.getProductId()))
                 .collect(Collectors.groupingBy(
                         DiscountInfoDto::getMartId,
-                        Collectors.summingLong(dto -> dto.getFinalPrice() * productIdToQuantity.get(dto.getProductId()))
+                        Collectors.summingLong(
+                                dto -> dto.getFinalPrice() * productIdToQuantity.get(dto.getProductId()))
                 ));
     }
 }
