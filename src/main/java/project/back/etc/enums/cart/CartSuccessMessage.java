18c0736c8dc2b7eb27b45f1d6b312765b9608e4c
leejuno <zhqkfxm0127@naver.com>
2024-05-20 21:43:57 +0900

    refactor(cart): enum에서 부자연스러운 문장 수정

--- src/main/java/project/back/etc/enums/cart/CartSuccessMessage.java
+++ src/main/java/project/back/etc/enums/cart/CartSuccessMessage.java
@@ -5,7 +5,7 @@ public enum CartSuccessMessage {
     SEARCH("상품 검색에 성공 했습니다."),
     ADD("장바구니에 '%s'이(가) 담겼습니다."),
     UPDATE("'%s'의 수량을 변경했습니다."),
-    DELETE("장바구니에서 '%s'를 삭제했습니다."),
+    DELETE("장바구니에서 '%s'을(를) 삭제했습니다."),
     DELETE_ALL("장바구니 비우기에 성공했습니다.");
 
     private String message;
