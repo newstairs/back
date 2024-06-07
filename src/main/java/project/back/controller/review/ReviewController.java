5e75abfdc7657b954a9bc9fe94e57635d8918edf
김정규 <111268088+gyu0918@users.noreply.github.com>
2024-06-07 18:53:00 +0900

    feat : ApiResponse적용

--- src/main/java/project/back/controller/review/ReviewController.java
+++ src/main/java/project/back/controller/review/ReviewController.java
@@ -3,6 +3,7 @@ package project.back.controller.review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import project.back.dto.ApiResponse;
 import project.back.dto.review.ReviewDto;
 import project.back.entity.review.Review;
 import project.back.service.review.ReviewService;
@@ -15,34 +16,34 @@ import java.util.List;
 public class ReviewController {
     private final ReviewService reviewService;
 
-    //리뷰  + 평점 조회 get
+    //리뷰 + 평점 조회 get
     @GetMapping("/{martId}")
-    public ResponseEntity<List<ReviewDto>> getReview(@PathVariable Long martId){
+    public ResponseEntity<ApiResponse<List<ReviewDto>>> getReview(@PathVariable Long martId){
         List<ReviewDto> reviews = reviewService.getReviewByMartId(martId);
         if (reviews.isEmpty()) {
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.ok(new ApiResponse<>(false, "No reviews found", null));
         }
-        return ResponseEntity.ok(reviews);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Reviews join successfully", reviews));
     }
 
     //리뷰 + 평점 작성 post
     @PostMapping("/")
-    public ResponseEntity<Review> writeReview(@RequestBody ReviewDto reviewDto){
+    public ResponseEntity<ApiResponse<Review>> writeReview(@RequestBody ReviewDto reviewDto){
         Review review = reviewService.writeReview(reviewDto);
-        return ResponseEntity.ok(review);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Review created successfully", review));
     }
 
     //리뷰 + 평점 삭제 delete
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<String> deleteReview(@PathVariable Long reviewId){
+    public ResponseEntity<ApiResponse<Void>> deleteReview(@PathVariable Long reviewId){
         reviewService.deleteReview(reviewId);
-        return ResponseEntity.ok("삭제 완료했습니다.");
+        return ResponseEntity.ok(new ApiResponse<>(true, "삭제 완료했습니다.", null));
     }
 
     //리뷰 + 평점 수정 patch
     @PatchMapping("/{reviewId}")
-    public ResponseEntity<ReviewDto> updateReview( @PathVariable Long reviewId,@RequestBody ReviewDto reviewDto) {
+    public ResponseEntity<ApiResponse<ReviewDto>> updateReview(@PathVariable Long reviewId, @RequestBody ReviewDto reviewDto) {
         ReviewDto updatedReview = reviewService.updateReview(reviewId, reviewDto.getReviewContent(), reviewDto.getScore());
-        return ResponseEntity.ok(updatedReview);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Review updated successfully", updatedReview));
     }
 }
