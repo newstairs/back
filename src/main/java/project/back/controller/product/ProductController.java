c93fb3fd1c2b16c6ed211d8a97582dd18c76a877
ExpeditionMoon <dnjswon1025@gmail.com>
2024-06-11 17:24:49 +0900

    refactor(product): 이미지 반환 형식 변경
    - Base64로 인코딩하여 반환 -> 이미지 URL 생성
    #76

--- src/main/java/project/back/controller/product/ProductController.java
+++ src/main/java/project/back/controller/product/ProductController.java
@@ -2,11 +2,7 @@ package project.back.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.back.dto.ApiResponse;
 import project.back.dto.cart.ProductSearchDto;
 import project.back.dto.product.ProductDto;
